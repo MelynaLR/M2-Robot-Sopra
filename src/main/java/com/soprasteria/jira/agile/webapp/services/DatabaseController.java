b1b84c3baf4c0ac580066a9ca595b0f54732dcf8
Phozel <salahahmednah@gmail.com>
2023-12-12 10:55:31 +0100

    added mysql dependency

--- src/main/java/com/soprasteria/jira/agile/webapp/services/DatabaseController.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/services/DatabaseController.java
@@ -1,37 +1,50 @@
 package com.soprasteria.jira.agile.webapp.services;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseController {
-    // Les informations de connexion à la base de données
-    private static final String URL = "jdbc:mysql://localhost:3306/jiraAPI";
-    private static final String UTILISATEUR = "admin";
-    private static final String MOT_DE_PASSE = "KOxNGMYzDuBKmYsNpdxP";
 
-    // Méthode pour établir une connexion à la base de données
+    // JDBC URL, username, and password of MySQL server
+    private static final String URL = "jdbc:mysql://localhost:3306/jiraAPI";
+    private static final String USER = "root";
+    private static final String PASSWORD = "KOxNGMYzDuBKmYsNpdxP";
+
+    // JDBC variables for opening, closing, and managing connection
+    private static Connection connection;
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, UTILISATEUR, MOT_DE_PASSE);
+        try {
+            // Load the JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish a connection
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 
-    // Exemple d'utilisation
+    // Other methods for interacting with the database go here...
+
+    // Example method to close the connection
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
     public static void main(String[] args) {
         try {
-            Connection connexion = getConnection();
-            if (connexion != null) {
-                System.out.println("Connexion réussie !");
-                // Faire d'autres opérations avec la base de données ici
-                // ...
-                // N'oubliez pas de fermer la connexion lorsque vous avez fini
-                connexion.close();
-            } else {
-                System.err.println("La connexion a échoué.");
-            }
+            // Example usage: establish a connection and do something
+            Connection conn = getConnection();
+            // ... perform database operations ...
+
+            // Close the connection when done
+            closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
-    //autres classes pour manipuler la bdd
 }
