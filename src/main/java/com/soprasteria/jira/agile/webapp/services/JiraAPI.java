26c67098ef9ef84e61db260aeb715c1eb6624351
yannisberry <berryyannis@gmail.com>
2023-12-12 16:06:44 +0100

    Mise en place recup API Jira

--- src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAPI.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAPI.java
@@ -1,39 +1,43 @@
 package com.soprasteria.jira.agile.webapp.services;
 
-import org.glassfish.jersey.client.ClientResponse;
 import java.util.Base64;
-import org.springframework.boot.autoconfigure.security.oauth2.server.servlet.OAuth2AuthorizationServerProperties.Client;
+
+import org.springframework.stereotype.Component;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class JiraAPI{
-	
-	
-	private String username = "berryyannis@gmail.com"; // a mettre dans .properties
-	private String tokenApi = "ATATT3xFfGF0W-C5fiUZ-EkE-St9eESZxHSyOuTi60rK5YJfufZPDiDaiWWNx05uUc8a8vv-0zs09IWUTrGjeiUs29uxMgDnftWX9jj0dXiAvdCru20h7Gel1SPwgKa-4YSx0qbYUcDEJyc67O1VSbTV-SEgahxnFAR_cNFWHgR7Pe4kLiopCAA=C572F4A4";
-	private String headerType = "application/json";
-	private String authorizationHeader;
-	
-	public void createAutorisationHeader() {
-		this.authorizationHeader = "Basic "+Base64.getEncoder().encodeToString(this.username.getBytes())+ " :"+this.tokenApi;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	 //String auth = new String(Base64.encode("username" + ":" + "password"));
-	 
-	/* 
-	 final String headerAuthorization = "Authorization";
-	 final String headerAuthorizationValue = "Basic " + auth;
-	 final String headerType = "application/json";
-	Client client = Client.create();
+@Component
+public class JiraAPI {
 
-	WebResource webResource = client.resource("url");
-	response =  webResource.header(headerAuthorization, headerAuthorizationValue).type(headerType).accept(headerType).get(ClientResponse.class);
-*/
+    private String username = "berryyannis@gmail.com";
+    private String tokenApi = "ATATT3xFfGF0W-C5fiUZ-EkE-St9eESZxHSyOuTi60rK5YJfufZPDiDaiWWNx05uUc8a8vv-0zs09IWUTrGjeiUs29uxMgDnftWX9jj0dXiAvdCru20h7Gel1SPwgKa-4YSx0qbYUcDEJyc67O1VSbTV-SEgahxnFAR_cNFWHgR7Pe4kLiopCAA=C572F4A4";
+    private String headerType = "application/json";
+    private String headerAuthorization = "Authorization";
+    private String headerValue;
+
+    public void createAuthorizationHeader() {
+        String value = this.username + ":" + this.tokenApi;
+        this.headerValue = "Basic " + Base64.getEncoder().encodeToString(value.getBytes());
+    }
+
+    public void sendRequestAPI(String urlAPI) {
+        Client client = Client.create();
+
+        WebResource webResource = client.resource(urlAPI);
+
+        ClientResponse response = webResource
+                .header(headerAuthorization, this.headerValue)
+                .type(this.headerType)
+                .accept(this.headerType)
+                .get(ClientResponse.class);
+
+        if (response.getStatus() == 200) {
+            String responseBody = response.getEntity(String.class);
+            System.out.println("Réponse de l'API : " + responseBody);
+        } else {
+            System.err.println("Erreur lors de la requête : " + response.getStatus());
+        }
+    }
 }
