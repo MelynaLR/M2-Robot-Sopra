cb1db8d6386919712881e750b5046b1b4c0dc245
yannisberry <berryyannis@gmail.com>
2023-12-12 09:33:12 +0100

    Initialisation application

---
+++ src/main/java/com/soprasteria/jira/agile/webapp/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.soprasteria.jira.agile.webapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class MainController{
+	@GetMapping(value="/")
+	public void retrieveData() {
+		System.out.println("coucou");		
+	}
+}
