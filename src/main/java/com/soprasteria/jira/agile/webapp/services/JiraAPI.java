180d5f25ff9b208dcc41385bb681ff74f9a91db8
Phozel <salahahmednah@gmail.com>
2023-12-13 10:40:57 +0100

    added dependency and retrieval/parsing of json information from JIRA API

--- src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAPI.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAPI.java
@@ -1,8 +1,10 @@
 package com.soprasteria.jira.agile.webapp.services;
 
-import java.util.Base64;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
+
+import java.util.Base64;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -35,9 +37,29 @@ public class JiraAPI {
 
         if (response.getStatus() == 200) {
             String responseBody = response.getEntity(String.class);
-            System.out.println("Réponse de l'API : " + responseBody);
+            System.out.println("Issues and their complexity points incoming : ");
+            parseJsonResponse(responseBody);
         } else {
             System.err.println("Erreur lors de la requête : " + response.getStatus());
+        }
+    }
+
+    private void parseJsonResponse(String responseBody) {
+        JSONObject json = new JSONObject(responseBody);
+
+        // Assuming the issues are stored in an array called "issues"
+        JSONArray issuesArray = json.getJSONArray("issues");
+
+        for (int i = 0; i < issuesArray.length(); i++) {
+            JSONObject issue = issuesArray.getJSONObject(i);
+            String issueName = issue.getJSONObject("fields").getString("summary");
+
+            // Replace "customfield_XXXXX" with the actual field ID for "user points" in Jira
+            int userPoints = issue.getJSONObject("fields").optInt("customfield_10032", -1);
+
+            // Print or process the issue name and "user points" as needed
+            System.out.println("Issue Name: " + issueName);
+            System.out.println("User Points: " + userPoints);
         }
     }
 }
