8037ecc68eeb8b7260f4cc07509922d3b211d446
yannisberry <berryyannis@gmail.com>
2024-01-15 14:56:34 +0100

    merge de requete-bdd

--- src/main/java/com/soprasteria/jira/agile/webapp/services/DataAnalysis.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/services/DataAnalysis.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/soprasteria/jira/agile/webapp/services/DataAnalysis.java
 package com.soprasteria.jira.agile.webapp.services;
 
 import java.util.ArrayList;
@@ -20,38 +19,5 @@ public class DataAnalysis{
 			}
 		}
 	}
-	
-	
-=======
-//package com.soprasteria.jira.agile.webapp.services;
-//import com.soprasteria.jira.agile.webapp.models.Issue_excel;
-//import com.soprasteria.jira.agile.webapp.services.dbExcel;
-//import java.util.ArrayList;
-//
-//import org.springframework.stereotype.Component;
-//
-//public class JiraAnalysis{
-//	
-//	private int userPointsScore = 100;
-//	
-//	public void calculateUserPointsScore (ArrayList<Issue_excel> issueList) {
-//		for (int i=0; i < issueList.size(); i++) {
-//			if (issueList.get(i).getUserPoints() >= 23) {
-//				// A TESTER / VOIR SI AUTRE CALCUL PLUS INTERESSANT
-//				this.userPointsScore = this.userPointsScore - 100/issueList.size();
-//			}
-//		}
-//	}
-//	
-//	
->>>>>>> requete-bdd:src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAnalysis.java
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
