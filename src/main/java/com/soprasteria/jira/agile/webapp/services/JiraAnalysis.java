b1270b9b13cc68d80d1b40a254009d14293cf6fb
claraadumas <clara.dumas@etu.univ-poitiers.fr>
2024-01-15 14:36:32 +0100

    Objet issue_excel Liste avec les données venant de MYSQL

--- src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAnalysis.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/services/JiraAnalysis.java
@@ -1,26 +1,24 @@
-package com.soprasteria.jira.agile.webapp.services;
-
-import java.util.ArrayList;
-
-import org.springframework.stereotype.Component;
-
-import com.soprasteria.jira.agile.webapp.models.Issue;
-
-@Component
-public class JiraAnalysis{
-	
-	private int userPointsScore = 100;
-	
-	public void calculateUserPointsScore (ArrayList<Issue> issueList) {
-		for (int i=0; i < issueList.size(); i++) {
-			if (issueList.get(i).getUserPoints() >= 23) {
-				// A TESTER / VOIR SI AUTRE CALCUL PLUS INTERESSANT
-				this.userPointsScore = this.userPointsScore - 100/issueList.size();
-			}
-		}
-	}
-	
-	
+//package com.soprasteria.jira.agile.webapp.services;
+//import com.soprasteria.jira.agile.webapp.models.Issue_excel;
+//import com.soprasteria.jira.agile.webapp.services.dbExcel;
+//import java.util.ArrayList;
+//
+//import org.springframework.stereotype.Component;
+//
+//public class JiraAnalysis{
+//	
+//	private int userPointsScore = 100;
+//	
+//	public void calculateUserPointsScore (ArrayList<Issue_excel> issueList) {
+//		for (int i=0; i < issueList.size(); i++) {
+//			if (issueList.get(i).getUserPoints() >= 23) {
+//				// A TESTER / VOIR SI AUTRE CALCUL PLUS INTERESSANT
+//				this.userPointsScore = this.userPointsScore - 100/issueList.size();
+//			}
+//		}
+//	}
+//	
+//	
 	
 	
 	
