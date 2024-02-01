8784ce30e1190b1f576b98a0b220795c56d5068d
yannisberry <berryyannis@gmail.com>
2024-02-01 10:52:40 +0100

    Ajout récupération champ sprintId

--- src/main/java/com/soprasteria/jira/agile/webapp/infrastructure/DatabaseInsertion.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/infrastructure/DatabaseInsertion.java
@@ -24,21 +24,22 @@ public class DatabaseInsertion {
             
             //Cette query insère la ligne mais si un issue du même nom existe déjà, elle met à jour ses points de complexité
             //String sql = "INSERT INTO issues (issueName, issueComplexity) VALUES (?, ?) ON DUPLICATE KEY UPDATE issueComplexity = ?";
-            String sql = "INSERT INTO issue (userName, description, creationDate, sprintEndDate, sprintId, sprintStartDate, Status, project_id, priority, userPoints) VALUES (?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE userPoints = ?";
+            String sql = "INSERT INTO issue (idIssue,userName, description, creationDate, sprintEndDate, sprintId, sprintStartDate, Status, project_id, priority, userPoints) VALUES (?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE userPoints = ?";
             //String sql = "INSERT INTO issues SET issueName=?, issueComplexity=?";
             //String sql = "INSERT INTO issues (issueName, issueComplexity) VALUES (?, ?)";
             preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, issue.getUser());
-            preparedStatement.setString(2, issue.getDescription());
-            preparedStatement.setString(3, issue.getCreationDate());
-            preparedStatement.setString(4, issue.getSprintEndDate());
-            preparedStatement.setString(5, issue.getSprintId());
-            preparedStatement.setString(6, issue.getSprintStartDate());
-            preparedStatement.setString(7, issue.getStatus());
-            preparedStatement.setString(8, issue.getProjectId());
-            preparedStatement.setString(9, issue.getPriority());
-            preparedStatement.setInt(10, issue.getUserPoints());
+            preparedStatement.setInt(1, issue.getId());
+            preparedStatement.setString(2, issue.getUser());
+            preparedStatement.setString(3, issue.getDescription());
+            preparedStatement.setString(4, issue.getCreationDate());
+            preparedStatement.setString(5, issue.getSprintEndDate());
+            preparedStatement.setString(6, issue.getSprintId());
+            preparedStatement.setString(7, issue.getSprintStartDate());
+            preparedStatement.setString(8, issue.getStatus());
+            preparedStatement.setString(9, issue.getProjectId());
+            preparedStatement.setString(10, issue.getPriority());
             preparedStatement.setInt(11, issue.getUserPoints());
+            preparedStatement.setInt(12, issue.getUserPoints());
 
             preparedStatement.executeUpdate();
 
