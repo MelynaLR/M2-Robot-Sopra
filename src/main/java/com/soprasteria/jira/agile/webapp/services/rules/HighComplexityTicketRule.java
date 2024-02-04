c3291d56255f28639374cd2ddb3b53d062d49b69
MelynaLR <melyna.lefevreromy@gmail.com>
2024-02-04 14:51:12 +0100

    add case of 0 issues

--- src/main/java/com/soprasteria/jira/agile/webapp/services/rules/HighComplexityTicketRule.java
+++ src/main/java/com/soprasteria/jira/agile/webapp/services/rules/HighComplexityTicketRule.java
@@ -28,8 +28,10 @@ public class HighComplexityTicketRule implements DataAnalysisRule{
 				rule.addIssue(issue);
 			}					
 		}
-		// returns the percentage of ok issues
-		rule.setScore((issues.size() - rule.getIssues().size()) * 100 / issues.size());
+		if (issues.size() != 0){
+			// returns the percentage of ok issues
+			rule.setScore((issues.size() - rule.getIssues().size()) * 100 / issues.size());
+		}		
 	}
 
 	@Override
